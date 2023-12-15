306baf2ca696262789169354743d28a32795eb8a
BrunoMatoszk <bruno.matos@sptech.school>
2023-12-15 15:10:48 -0300

    Back-end da aplicação

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -28,7 +28,7 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        camaras: resultadoAquarios
+                                        camaras: resultadoAquarios[0].id
                                     });
                                 } else {
                                     res.status(204).json({ camaras: [] });
